23e50d7859b54c0ae6932c2d04dac332a34c00f8
jeanvania <iermolenko@kameleoon.com>
2021-01-21 22:10:31 +0300

    adding todoList/todoForm components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { TodoForm } from './components/todoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addHandler = (title: string) => {
+    console.log('Add New Todo', title)
+  }
+  return <>
+    <Navbar />
+    <div className="container">
+      <TodoForm onAdd={addHandler} />
     </div>
-  );
+  </>
 }
 
 export default App;
